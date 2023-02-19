1842ea4d02c906feca01c4844b9222ab4e859081
Finn <finnfassnacht@gmail.com>
2023-02-19 13:21:18 +0100

    add coments to LLM py script

--- LLM.py
+++ LLM.py
@@ -1,6 +1,6 @@
-from transformers import pipeline # First line
-import sys
-prompt = sys.argv[1]
-generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M') # Second line
-res = generator(prompt, max_length=20, do_sample=True, temperature=0.9,  pad_token_id=50256) # Fourth line
-print(res[0]["generated_text"])
+from transformers import pipeline # import transformers
+import sys 
+prompt = sys.argv[1] # take terminal args
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-125M') # specify wich LLM to use
+res = generator(prompt, max_length=20, do_sample=True, temperature=0.9,  pad_token_id=50256) # generate 
+print(res[0]["generated_text"]) # print response
