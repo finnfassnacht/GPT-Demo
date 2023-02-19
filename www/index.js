7de46cb18c16ceeb5a6aa4c6bfca5d5126ef69b0
Finn <finnfassnacht@gmail.com>
2023-02-19 14:46:46 +0100

    update css and html, index.js fetch and display LLM response

---
+++ www/index.js
@@ -0,0 +1,15 @@
+function user_send(){
+    let prompt = document.getElementById("user_in").value;
+    async function fetcher(prompt){
+        const response = await fetch("/maschine/LLM/" + prompt)
+        const data = await response.json()
+        if (data.err !== undefined){
+            document.getElementById("text").innerText = "Internal error (this is not generated)"
+        }
+        else{
+            let llm_res = data.llm
+            document.getElementById("text").innerText = llm_res;
+        }
+    }
+    fetcher(prompt)
+}
